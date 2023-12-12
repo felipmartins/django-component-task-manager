69af5c68baee8250b5041c1c8e1ad94874ebe12c
felipmartins <felipmartins94@gmail.com>
2023-12-12 13:15:46 -0300

    adiciona rotas

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from tasks.views import index
 
 
-urlpatterns = []
+urlpatterns = [
+    path("", index, name="tasks-page"),
+    path("login/", index, name="login-page"),
+]
