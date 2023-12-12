2bd7fb85dc392a3d265a71c3d910dcaedba0be8f
felipmartins <felipmartins94@gmail.com>
2023-12-12 13:08:40 -0300

    constroi o modelo base para `tasks`

---
+++ tasks/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from uuid import uuid4
+from django.contrib.auth.models import User
+
+
+class Task(models.Model):
+    uuid = models.UUIDField(
+        default=uuid4,
+        editable=False,
+        unique=True,
+        primary_key=True,
+    )
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return (
+            f"{self.title} - {self.user} - Pendente ⏱️"
+            if not self.is_completed
+            else f"{self.title} - {self.user} - Concluída ✅"
+        )
