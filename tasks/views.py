7cf2ba8f09158b540c33e4ef1bc48fa7ac61424b
felipmartins <felipmartins94@gmail.com>
2023-12-12 21:38:50 -0300

    implementação das views

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from tasks.models import User
+from tasks.forms import LoginForm
 
 
 def index(request):
+    if "user_id" not in request.session:
+        return redirect("login-page")
+
     return render(request, "index.html")
 
 
 def visitor_login(request):
-    return render(request, "login.html")
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            user = User.objects.get_or_create(**form.cleaned_data)[0]
+            request.session["user_id"] = user.id
+            return redirect("tasks-page")
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "login.html", context)
